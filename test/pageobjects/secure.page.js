c764d99a5e21c3f9386f4d5a555f74bfbe5dda90
MacMini-23 <macmini-23@MacMini-23s-Mac-mini.local>
2023-03-23 15:57:02 -0700

    Project 6 Organize Test

--- test/pageobjects/secure.page.js
+++ test/pageobjects/secure.page.js
@@ -12,6 +12,13 @@ class SecurePage extends Page {
     get flashAlert () {
         return $('#flash');
     }
+
+    get btnLogout() {
+        return $('a.button');
+    }
+    async logout() {
+        await this.btnLogout.click();
+    }
 }
 
 export default new SecurePage();
---
+++ test/specs/login.spec.js
@@ -0,0 +1,38 @@
+// const LoginPage = require('../pageobjects/login.page');
+// const SecurePage = require('../pageobjects/secure.page');
+import LoginPage from '../pageobjects/login.page.js'
+import SecurePage from '../pageobjects/secure.page.js'
+
+
+describe('My Login application', () => {
+    describe('can login and log out', () => {
+    xit('should login with valid credentials', async () => {
+        await LoginPage.open();
+
+        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+        await expect(SecurePage.flashAlert).toBeExisting();
+        await expect(SecurePage.flashAlert).toHaveTextContaining(
+            'You logged into a secure area!');
+    });
+    // my new test here
+    xit('should fail to login with invalid credentials', async () => {
+        await LoginPage.open();
+
+        await LoginPage.login('tomsmith', 'wrong');
+        await expect(LoginPage.flashAlert).toBeExisting();
+        await expect(LoginPage.flashAlert).toHaveTextContaining(
+            'Your password is invalid!');
+    });
+
+    it('should logout', async () => {
+        await LoginPage.open();
+
+        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
+        await expect(browser).toHaveUrl('https://the-internet.herokuapp.com/secure');
+        
+        await SecurePage.logout();
+        await expect(browser).toHaveUrl('https://the-internet.herokuapp.com/login');
+    });
+});
+
+});
